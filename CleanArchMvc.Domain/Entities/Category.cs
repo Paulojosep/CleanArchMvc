a915c301bea3216713ebca06953b3898a3d0e573
Paulo Modesto <paulo.jmodesto@hotmail.com>
2021-10-31 23:22:43 -0300

    Entidades created

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; private set; }
+    }
+}
