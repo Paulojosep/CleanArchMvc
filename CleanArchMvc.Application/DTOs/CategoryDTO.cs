96fbe28a32334a6e9885fa825bdd91bc7f49f81e
Paulo Modesto <paulo.jmodesto@hotmail.com>
2021-11-14 17:11:03 -0300

    Criação das Entidades DTOs e os mapeamentos

---
+++ CleanArchMvc.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name is Required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
